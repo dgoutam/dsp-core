ad863b761c75fbcb00b428be3ba2364f4529d75a
Lee Hicks <leehicks@dreamfactory.com>
2012-12-12 17:40:57 -0500

    setup local storage

--- scripts/install-web-site.sh
+++ scripts/install-web-site.sh
@@ -11,8 +11,9 @@ if [ "." == "${BASE_PATH}" ] ; then
 fi
 
 LOG_DIR="${BASE_PATH}/log/"
-WEB_DIR="${BASE_PATH}/web"
+STORAGE_DIR="${BASE_PATH}/storage/"
 VENDOR_DIR="${BASE_PATH}/vendor"
+WEB_DIR="${BASE_PATH}/web"
 PUBLIC_DIR="${WEB_DIR}/public"
 ASSETS_DIR="${PUBLIC_DIR}/assets"
 
@@ -21,8 +22,9 @@ Paths
 --------
 Base	:	${BASE_PATH}
 Log     :	${LOG_DIR}
-Web	    :	${WEB_DIR}
+Storage	:	${STORAGE_DIR}
 Vendor	:	${VENDOR_DIR}
+Web	    :	${WEB_DIR}
 Public	:	${PUBLIC_DIR}
 Assets	:	${ASSETS_DIR}
 
@@ -30,6 +32,10 @@ Assets	:	${ASSETS_DIR}
 
 if [ ! -d "${LOG_DIR}" ] ; then
 	mkdir "${LOG_DIR}" >/dev/null 2>&1
+fi
+
+if [ ! -d "${STORAGE_DIR}" ] ; then
+	mkdir "${STORAGE_DIR}" >/dev/null 2>&1
 fi
 
 if [ ! -d "${ASSETS_DIR}" ] ; then
@@ -44,10 +50,12 @@ if [ ! -d "${PUBLIC_DIR}/launchpad" ] ; then
 	ln -s "${VENDOR_DIR}/dreamfactory/app-launchpad/" "${PUBLIC_DIR}/launchpad" >/dev/null 2>&1
 fi
 
-chgrp -R www-data "${LOG_DIR}" "${PUBLIC_DIR}"
+# make writable by web-server, change www-data to _www on Mac, see 'cat /etc/apache2/httpd.conf'
+chgrp -R www-data "${LOG_DIR}" "${STORAGE_DIR}" "${PUBLIC_DIR}"
 chmod -R 2755 "${PUBLIC_DIR}"
 chmod -R 2775 "${ASSETS_DIR}" 
 chmod -R 2775 "${LOG_DIR}" 
+chmod -R 2775 "${STORAGE_DIR}"
 
 echo "Complete."
 echo
