facccac221d1ba36a0962854a894873b44e5ad6d
Jerry Ablan <jerryablan@dreamfactory.com>
2013-01-21 12:42:50 -0500

    Added a class to talk to Drupal API

--- web/protected/controllers/AppController.php
+++ web/protected/controllers/AppController.php
@@ -1,5 +1,4 @@
 <?php
-
 /**
  *
  */
@@ -9,7 +8,7 @@ class AppController extends Controller
 	 * @var string the default layout for the views. Defaults to '//layouts/column2', meaning
 	 * using two-column layout. See 'protected/views/layouts/column2.php'.
 	 */
-	public $layout='//layouts/column2';
+	public $layout = '//layouts/column2';
 
 	/**
 	 * @return array action filters
@@ -25,54 +24,65 @@ class AppController extends Controller
 	/**
 	 * Specifies the access control rules.
 	 * This method is used by the 'accessControl' filter.
+	 *
 	 * @return array access control rules
 	 */
 	public function accessRulesx()
 	{
 		return array(
-			array('allow',  // allow all users to perform 'index' and 'view' actions
-				'actions'=>array('index','view'),
-				'users'=>array('*'),
+			array(
+				'allow', // allow all users to perform 'index' and 'view' actions
+				'actions' => array( 'index', 'view' ),
+				'users'   => array( '*' ),
 			),
-			array('allow', // allow authenticated user to perform 'create' and 'update' actions
-				'actions'=>array('create','update'),
-				'users'=>array('@'),
+			array(
+				'allow', // allow authenticated user to perform 'create' and 'update' actions
+				'actions' => array( 'create', 'update' ),
+				'users'   => array( '@' ),
 			),
-			array('allow', // allow admin user to perform 'admin' and 'delete' actions
-				'actions'=>array('admin','delete'),
-				'users'=>array('admin'),
+			array(
+				'allow', // allow admin user to perform 'admin' and 'delete' actions
+				'actions' => array( 'admin', 'delete' ),
+				'users'   => array( 'admin' ),
 			),
-			array('deny',  // deny all users
-				'users'=>array('*'),
+			array(
+				'deny', // deny all users
+				'users' => array( '*' ),
 			),
 		);
 	}
 
-    /**
-     *
-     */
-    public function actionStream()
-    {
-        $path = (isset($_GET['path']) ? $_GET['path'] : '');
-        try {
-            $app = ServiceHandler::getInstance()->getServiceObject('app');
-            $app->streamFile($path);
-            Yii::app()->end();
-        }
-        catch (\Exception $ex) {
-            die($ex->getMessage());
-        }
-    }
+	/**
+	 *
+	 */
+	public function actionStream()
+	{
+		$path = ( isset( $_GET['path'] ) ? $_GET['path'] : '' );
+		try
+		{
+			$app = ServiceHandler::getInstance()->getServiceObject( 'app' );
+			$app->streamFile( $path );
+			Yii::app()->end();
+		}
+		catch ( \Exception $ex )
+		{
+			die( $ex->getMessage() );
+		}
+	}
 
 	/**
 	 * Displays a particular model.
+	 *
 	 * @param integer $id the ID of the model to be displayed
 	 */
-	public function actionView($id)
+	public function actionView( $id )
 	{
-		$this->render('view',array(
-			'model'=>$this->loadModel($id),
-		));
+		$this->render(
+			'view',
+			array(
+				 'model' => $this->loadModel( $id ),
+			)
+		);
 	}
 
 	/**
@@ -81,59 +91,73 @@ class AppController extends Controller
 	 */
 	public function actionCreate()
 	{
-		$model=new App;
+		$model = new App;
 
 		// Uncomment the following line if AJAX validation is needed
 		// $this->performAjaxValidation($model);
 
-		if(isset($_POST['App']))
+		if ( isset( $_POST['App'] ) )
 		{
-			$model->attributes=$_POST['App'];
-			if($model->save())
-				$this->redirect(array('view','id'=>$model->id));
+			$model->attributes = $_POST['App'];
+			if ( $model->save() )
+			{
+				$this->redirect( array( 'view', 'id' => $model->id ) );
+			}
 		}
 
-		$this->render('create',array(
-			'model'=>$model,
-		));
+		$this->render(
+			'create',
+			array(
+				 'model' => $model,
+			)
+		);
 	}
 
 	/**
 	 * Updates a particular model.
 	 * If update is successful, the browser will be redirected to the 'view' page.
+	 *
 	 * @param integer $id the ID of the model to be updated
 	 */
-	public function actionUpdate($id)
+	public function actionUpdate( $id )
 	{
-		$model=$this->loadModel($id);
+		$model = $this->loadModel( $id );
 
 		// Uncomment the following line if AJAX validation is needed
 		// $this->performAjaxValidation($model);
 
-		if(isset($_POST['App']))
+		if ( isset( $_POST['App'] ) )
 		{
-			$model->attributes=$_POST['App'];
-			if($model->save())
-				$this->redirect(array('view','id'=>$model->id));
+			$model->attributes = $_POST['App'];
+			if ( $model->save() )
+			{
+				$this->redirect( array( 'view', 'id' => $model->id ) );
+			}
 		}
 
-		$this->render('update',array(
-			'model'=>$model,
-		));
+		$this->render(
+			'update',
+			array(
+				 'model' => $model,
+			)
+		);
 	}
 
 	/**
 	 * Deletes a particular model.
 	 * If deletion is successful, the browser will be redirected to the 'admin' page.
+	 *
 	 * @param integer $id the ID of the model to be deleted
 	 */
-	public function actionDelete($id)
+	public function actionDelete( $id )
 	{
-		$this->loadModel($id)->delete();
+		$this->loadModel( $id )->delete();
 
 		// if AJAX request (triggered by deletion via admin grid view), we should not redirect the browser
-		if(!isset($_GET['ajax']))
-			$this->redirect(isset($_POST['returnUrl']) ? $_POST['returnUrl'] : array('admin'));
+		if ( !isset( $_GET['ajax'] ) )
+		{
+			$this->redirect( isset( $_POST['returnUrl'] ) ? $_POST['returnUrl'] : array( 'admin' ) );
+		}
 	}
 
 	/**
@@ -141,10 +165,13 @@ class AppController extends Controller
 	 */
 	public function actionIndex()
 	{
-		$dataProvider=new CActiveDataProvider('App');
-		$this->render('index',array(
-			'dataProvider'=>$dataProvider,
-		));
+		$dataProvider = new CActiveDataProvider( 'App' );
+		$this->render(
+			'index',
+			array(
+				 'dataProvider' => $dataProvider,
+			)
+		);
 	}
 
 	/**
@@ -152,38 +179,48 @@ class AppController extends Controller
 	 */
 	public function actionAdmin()
 	{
-		$model=new App('search');
-		$model->unsetAttributes();  // clear any default values
-		if(isset($_GET['App']))
-			$model->attributes=$_GET['App'];
+		$model = new App( 'search' );
+		$model->unsetAttributes(); // clear any default values
+		if ( isset( $_GET['App'] ) )
+		{
+			$model->attributes = $_GET['App'];
+		}
 
-		$this->render('admin',array(
-			'model'=>$model,
-		));
+		$this->render(
+			'admin',
+			array(
+				 'model' => $model,
+			)
+		);
 	}
 
 	/**
 	 * Returns the data model based on the primary key given in the GET variable.
 	 * If the data model is not found, an HTTP exception will be raised.
+	 *
 	 * @param integer the ID of the model to be loaded
 	 */
-	public function loadModel($id)
+	public function loadModel( $id )
 	{
-		$model=App::model()->findByPk($id);
-		if($model===null)
-			throw new CHttpException(404,'The requested page does not exist.');
+		$model = App::model()->findByPk( $id );
+		if ( $model === null )
+		{
+			throw new CHttpException( 404, 'The requested page does not exist.' );
+		}
+
 		return $model;
 	}
 
 	/**
 	 * Performs the AJAX validation.
+	 *
 	 * @param CModel the model to be validated
 	 */
-	protected function performAjaxValidation($model)
+	protected function performAjaxValidation( $model )
 	{
-		if(isset($_POST['ajax']) && $_POST['ajax']==='app-form')
+		if ( isset( $_POST['ajax'] ) && $_POST['ajax'] === 'app-form' )
 		{
-			echo CActiveForm::validate($model);
+			echo CActiveForm::validate( $model );
 			Yii::app()->end();
 		}
 	}
