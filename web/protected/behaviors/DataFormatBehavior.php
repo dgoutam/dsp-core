66006d0e84dd07d7e2249d26180377d4f98ab19a
Jerry Ablan <jerryablan@dreamfactory.com>
2013-03-19 11:04:57 -0400

    WIP

---
+++ web/protected/behaviors/DataFormatBehavior.php
@@ -0,0 +1,278 @@
+<?php
+/**
+ * BaseDspModelBehavior.php
+ * A base class for AR behaviors
+ *
+ * This file is part of the DreamFactory Services Platform(tm) (DSP)
+ * Copyright (c) 2012-2013 DreamFactory Software, Inc. <developer-support@dreamfactory.com>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * Defines two "built-in" behaviors: DataFormat and TimeStamp
+ *  - DataFormat automatically formats date/time values for the target database platform (MySQL, Oracle, etc.)
+ *  - TimeStamp automatically updates create_date and lmod_date columns in tables upon save.
+ *
+ * @property int    $id
+ * @property string $created_date
+ * @property string $last_modified_date
+ */
+/**
+ * DataFormatBehavior
+ * If attached to a model, fields are formatted per your configuration. Also provides a default sort for a model
+ */
+class DataFormatBehavior extends BaseDspModelBehavior
+{
+	//********************************************************************************
+	//* Member
+	//********************************************************************************
+
+	/***
+	 * Holds the default/configured formats for use when populating fields
+	 *
+	 * array(
+	 *     'event' => array(                //    The event to apply format in
+	 *         'dataType' => <format>        //    The format for the display
+	 *         'method' => <function>        //    The function to call for formatting
+	 *     ),                                //        Send array(object,method) for class methods
+	 *     'event' => array(                //    The event to apply format in
+	 *         'dataType' => <format>        //    The format for the display
+	 *         'method' => <function>        //    The function to call for formatting
+	 *     ),                                //        Send array(object,method) for class methods
+	 *     ...
+	 *
+	 * @var array
+	 */
+	protected $_dateFormat
+		= array(
+			'afterFind'     => array(
+				'date'     => 'm/d/Y',
+				'datetime' => 'm/d/Y H:i:s',
+			),
+			'afterValidate' => array(
+				'date'     => 'Y-m-d',
+				'datetime' => 'Y-m-d H:i:s',
+			),
+		);
+
+	/**
+	 * @var string The default sort order
+	 */
+	protected $_defaultSort;
+
+	//*************************************************************************
+	//* Handlers
+	//*************************************************************************
+
+	/**
+	 * Apply any formats
+	 *
+	 * @param \CModelEvent event parameter
+	 *
+	 * @return bool|void
+	 */
+	public function beforeValidate( $event )
+	{
+		return $this->_handleEvent( __FUNCTION__, $event );
+	}
+
+	/**
+	 * Apply any formats
+	 *
+	 * @param \CEvent $event
+	 *
+	 * @return bool|void
+	 */
+	public function afterValidate( $event )
+	{
+		return $this->_handleEvent( __FUNCTION__, $event );
+	}
+
+	/**
+	 * Apply any formats
+	 *
+	 * @param \CEvent $event
+	 *
+	 * @return bool|void
+	 */
+	public function beforeFind( $event )
+	{
+		//	Is a default sort defined?
+		if ( $this->_defaultSort )
+		{
+			//	Is a sort defined?
+			$_criteria = $event->sender->getDbCriteria();
+			/** @var $_model \CActiveRecord */
+			$_model = $event->sender;
+
+			//	No sort? Set the default
+			if ( !$_criteria->order )
+			{
+				$_model->getDbCriteria()->mergeWith(
+					new CDbCriteria( array(
+										  'order' => $this->_defaultSort,
+									 ) )
+				);
+			}
+		}
+
+		return $this->_handleEvent( __FUNCTION__, $event );
+	}
+
+	/**
+	 * Apply any formats
+	 *
+	 * @param \CEvent $event
+	 *
+	 * @return bool|void
+	 */
+	public function afterFind( $event )
+	{
+		return $this->_handleEvent( __FUNCTION__, $event );
+	}
+
+	//*************************************************************************
+	//* Methods
+	//*************************************************************************
+
+	/**
+	 * @param string $which
+	 * @param string $type
+	 *
+	 * @return string
+	 */
+	public function getFormat( $which = 'afterFind', $type = 'date' )
+	{
+		return \Kisma\Core\Utility\Scalar::nvl( $this->_dateFormat[$which][$type], 'm/d/Y' );
+	}
+
+	/**
+	 * Sets a format
+	 *
+	 * @param string $which
+	 * @param string $type
+	 * @param string $format
+	 */
+	public function setFormat( $which = 'afterValidate', $type = 'date', $format = 'm/d/Y' )
+	{
+		if ( !isset( $this->_dateFormat[$which] ) )
+		{
+			$this->_dateFormat[$which] = array();
+		}
+
+		$this->_dateFormat[$which][$type] = $format;
+	}
+
+	/**
+	 * @param string $defaultSort
+	 *
+	 * @return DataFormatBehavior
+	 */
+	public function setDefaultSort( $defaultSort )
+	{
+		$this->_defaultSort = $defaultSort;
+
+		return $this;
+	}
+
+	/**
+	 * @return string
+	 */
+	public function getDefaultSort()
+	{
+		return $this->_defaultSort;
+	}
+
+	/**
+	 * Applies the requested format to the value and returns it.
+	 * Override this method to apply additional format types.
+	 *
+	 * @param \CDbColumnSchema $column
+	 * @param mixed            $value
+	 * @param string           $which
+	 *
+	 * @return mixed
+	 */
+	protected function _applyFormat( $column, $value, $which = 'view' )
+	{
+		$_result = null;
+
+		//	Apply formats
+		switch ( $column->dbType )
+		{
+			case 'date':
+			case 'datetime':
+			case 'timestamp':
+				//	Handle blanks
+				if ( null != $value && $value != '0000-00-00' && $value != '0000-00-00 00:00:00' )
+				{
+					$_result = date( $this->getFormat( $which, $column->dbType ), strtotime( $value ) );
+				}
+				break;
+
+			default:
+				$_result = $value;
+				break;
+		}
+
+		return $_result;
+	}
+
+	/**
+	 * Process the data and apply formats
+	 *
+	 * @param string  $which
+	 * @param \CEvent $event
+	 *
+	 * @return bool
+	 */
+	protected function _handleEvent( $which, \CEvent $event )
+	{
+		static $_schema;
+		static $_schemaFor;
+
+		$_model = $event->sender;
+
+		//	Cache for multi event speed
+		if ( $_schemaFor != get_class( $_model ) )
+		{
+			$_schema = $_model->getMetaData()->columns;
+			$_schemaFor = get_class( $_model );
+		}
+
+		//	Not for us? Pass it through...
+		if ( isset( $this->_dateFormat[$which] ) )
+		{
+			//	Is it safe?
+			if ( !$_schema )
+			{
+				$_model->addError( null, 'Cannot read schema for data formatting' );
+
+				return false;
+			}
+
+			//	Scoot through and update values...
+			foreach ( $_schema as $_name => $_column )
+			{
+				if ( !empty( $_name ) && $_model->hasAttribute( $_name ) && isset( $_schema[$_name], $this->_dateFormat[$which][$_column->dbType] ) )
+				{
+					$_value = $this->_applyFormat( $_column, $_model->getAttribute( $_name ), $which );
+					$_model->setAttribute( $_name, $_value );
+				}
+			}
+		}
+
+		//	Papa don't preach...
+		return parent::$which( $event );
+	}
+}
---
+++ web/protected/behaviors/TimestampBehavior.php
@@ -0,0 +1,277 @@
+<?php
+/**
+ * TimestampBehavior.php
+ * Allows you to define time stamp fields in models and have them automatically updated.
+ *
+ * This file is part of the DreamFactory Services Platform(tm) (DSP)
+ * Copyright (c) 2012-2013 DreamFactory Software, Inc. <developer-support@dreamfactory.com>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * Defines two "built-in" behaviors: DataFormat and TimeStamp
+ *  - DataFormat automatically formats date/time values for the target database platform (MySQL, Oracle, etc.)
+ *  - TimeStamp automatically updates create_date and lmod_date columns in tables upon save.
+ *
+ * @property string $createdColumn    The name of the column that holds your create date
+ * @property string $createdByColumn  The name of the column that holds your creating user
+ * @property string $lmodColumn       The name of the column that holds your last modified date
+ * @property string $lmodByColumn     The name of the column that holds your last modifying user
+ * @property string $dateTimeFunction The name of the function to use to set dates. Defaults to date('Y-m-d H:i:s').
+ */
+class TimestampBehavior extends BaseDspModelBehavior
+{
+	//********************************************************************************
+	//* Members
+	//********************************************************************************
+
+	/**
+	 * @var string The optional name of the create date column
+	 */
+	protected $_createdColumn = null;
+	/**
+	 * @var string The optional name of the created by user id column
+	 */
+	protected $_createdByColumn = null;
+	/**
+	 * @var string The optional name of the last modified date column
+	 */
+	protected $_lastModifiedColumn = null;
+	/**
+	 * @var string The optional name of the last modified by user id column
+	 */
+	protected $_lastModifiedByColumn = null;
+	/**
+	 * @var string The date/time format to use if not using $dateTimeFunction
+	 */
+	protected $_dateTimeFormat = 'Y-m-d H:i:s';
+	/**
+	 * @var string The date/time with which function to stamp records
+	 */
+	protected $_dateTimeFunction = null;
+
+	//********************************************************************************
+	//*  Handlers
+	//********************************************************************************
+
+	/**
+	 * Timestamps row
+	 *
+	 * @param \CModelEvent $event
+	 */
+	public function beforeValidate( $event )
+	{
+		//	Handle created stamp
+		if ( $event->sender->isNewRecord )
+		{
+			if ( $this->_createdColumn && $event->sender->hasAttribute( $this->_createdColumn ) )
+			{
+				$this->owner->setAttribute(
+					$this->_createdColumn,
+					( null === $this->_dateTimeFunction ) ? date( $this->_dateTimeFormat ) : eval( 'return ' . $this->_dateTimeFunction . ';' )
+				);
+			}
+
+			if ( $this->_createdByColumn && $event->sender->hasAttribute( $this->_createdByColumn ) && !$event->sender->getAttribute(
+				$this->_createdByColumn
+			)
+			)
+			{
+				$this->owner->setAttribute( $this->_createdByColumn, SessionManager::getCurrentUserId() );
+			}
+		}
+
+		//	Handle lmod stamp
+		if ( $this->_lastModifiedColumn && $event->sender->hasAttribute( $this->_lastModifiedColumn ) )
+		{
+			$this->owner->setAttribute(
+				$this->_lastModifiedColumn,
+				( null === $this->_dateTimeFunction ) ? date( $this->_dateTimeFormat ) : eval( 'return ' . $this->_dateTimeFunction . ';' )
+			);
+		}
+
+		//	Handle user id stamp
+		if ( $this->_lastModifiedByColumn && $event->sender->hasAttribute( $this->_lastModifiedByColumn ) && !$event->sender->getAttribute(
+			$this->_lastModifiedByColumn
+		)
+		)
+		{
+			$this->owner->setAttribute( $this->_lastModifiedByColumn, SessionManager::getCurrentUserId() );
+		}
+
+		parent::beforeValidate( $event );
+	}
+
+	//********************************************************************************
+	//* Public Methods
+	//********************************************************************************
+
+	/**
+	 * Sets lmod date(s) and saves
+	 * Will optionally touch other columns. You can pass in a single column name or an array of columns.
+	 * This is useful for updating not only the lmod column but a last login date for example.
+	 * Only the columns that have been touched are updated. If no columns are updated, no database action is performed.
+	 *
+	 * @param mixed $additionalColumns The single column name or array of columns to touch in addition to configured lmod column
+	 *
+	 * @return boolean
+	 */
+	public function touch( $additionalColumns = null )
+	{
+		$_touchValue = ( null === $this->_dateTimeFunction ) ? date( $this->_dateTimeFormat ) : eval( 'return ' . $this->_dateTimeFunction . ';' );
+		$_updateList = array();
+
+		//	Any other columns to touch?
+		if ( null !== $additionalColumns )
+		{
+			foreach ( \Kisma\Core\Utility\Option::clean( $additionalColumns ) as $_attribute )
+			{
+				if ( $this->owner->hasAttribute( $_attribute ) )
+				{
+					$this->owner->setAttribute( $_attribute, $_touchValue );
+					$_updateList[] = $_attribute;
+				}
+			}
+		}
+
+		if ( $this->_lastModifiedColumn && $this->owner->hasAttribute( $this->_lastModifiedColumn ) )
+		{
+			$this->owner->setAttribute( $this->_lastModifiedColumn, $_touchValue );
+			$_updateList[] = $this->_lastModifiedColumn;
+		}
+
+		//	Only update if and what we've touched...
+		return count( $_updateList ) ? $this->owner->update( $_updateList ) : true;
+	}
+
+	/**
+	 * @param string $createdByColumn
+	 *
+	 * @return TimestampBehavior
+	 */
+	public function setCreatedByColumn( $createdByColumn )
+	{
+		$this->_createdByColumn = $createdByColumn;
+
+		return $this;
+	}
+
+	/**
+	 * @return string
+	 */
+	public function getCreatedByColumn()
+	{
+		return $this->_createdByColumn;
+	}
+
+	/**
+	 * @param string $createdColumn
+	 *
+	 * @return TimestampBehavior
+	 */
+	public function setCreatedColumn( $createdColumn )
+	{
+		$this->_createdColumn = $createdColumn;
+
+		return $this;
+	}
+
+	/**
+	 * @return string
+	 */
+	public function getCreatedColumn()
+	{
+		return $this->_createdColumn;
+	}
+
+	/**
+	 * @param string $dateTimeFunction
+	 *
+	 * @return TimestampBehavior
+	 */
+	public function setDateTimeFunction( $dateTimeFunction )
+	{
+		$this->_dateTimeFunction = $dateTimeFunction;
+
+		return $this;
+	}
+
+	/**
+	 * @return string
+	 */
+	public function getDateTimeFunction()
+	{
+		return $this->_dateTimeFunction;
+	}
+
+	/**
+	 * @param string $lastModifiedByColumn
+	 *
+	 * @return TimestampBehavior
+	 */
+	public function setLastModifiedByColumn( $lastModifiedByColumn )
+	{
+		$this->_lastModifiedByColumn = $lastModifiedByColumn;
+
+		return $this;
+	}
+
+	/**
+	 * @return string
+	 */
+	public function getLastModifiedByColumn()
+	{
+		return $this->_lastModifiedByColumn;
+	}
+
+	/**
+	 * @param string $lastModifiedColumn
+	 *
+	 * @return TimestampBehavior
+	 */
+	public function setLastModifiedColumn( $lastModifiedColumn )
+	{
+		$this->_lastModifiedColumn = $lastModifiedColumn;
+
+		return $this;
+	}
+
+	/**
+	 * @return string
+	 */
+	public function getLastModifiedColumn()
+	{
+		return $this->_lastModifiedColumn;
+	}
+
+	/**
+	 * @param string $dateTimeFormat
+	 *
+	 * @return TimestampBehavior
+	 */
+	public function setDateTimeFormat( $dateTimeFormat )
+	{
+		$this->_dateTimeFormat = $dateTimeFormat;
+
+		return $this;
+	}
+
+	/**
+	 * @return string
+	 */
+	public function getDateTimeFormat()
+	{
+		return $this->_dateTimeFormat;
+	}
+
+}
