13b0a8900343ee73bd50a68cffd547b5fe0e67b5
lucifurious <jerryablan@gmail.com>
2013-03-18 20:48:02 -0400

    Speed improvements

--- web/protected/controllers/LibController.php
+++ web/protected/controllers/LibController.php
@@ -6,58 +6,63 @@
 class LibController extends Controller
 {
 
-    /**
-     * @return array action filters
-     */
-    public function filters()
-    {
-        return array();
-    }
+	/**
+	 * @return array action filters
+	 */
+	public function filters()
+	{
+		return array();
+	}
 
-    // Actions
-    /**
-     *
-     */
-    public function actionStream()
-    {
-        $path = (isset($_GET['path']) ? $_GET['path'] : '');
-        try {
-            if (0 === substr_compare($path, 'web-core/', 0, 9, true)) {
-                $key = Yii::app()->basePath . DIRECTORY_SEPARATOR . '..' . DIRECTORY_SEPARATOR;
-                $key .= 'public' . DIRECTORY_SEPARATOR . $path;
-                if (is_file($key)) {
-                    $ext = FileUtilities::getFileExtension($key);
-                    $result = file_get_contents($key);
-                    header('Last-Modified: ' . gmdate('D, d M Y H:i:s \G\M\T', filemtime($key)));
-                    header('Content-type: ' . FileUtilities::determineContentType($ext, '', $key));
-                    header('Content-Length:' . filesize($key));
-                    $disposition = 'inline';
-                    header("Content-Disposition: $disposition; filename=\"$path\";");
-                    echo $result;
-                }
-                else {
-                    $status_header = "HTTP/1.1 404 The specified file '$path' does not exist.";
-                    header($status_header);
-                    header('Content-type: text/html');
-                }
-            }
-            else {
-                $app = ServiceHandler::getInstance()->getServiceObject('lib');
-                $app->streamFile($path);
-            }
-            Yii::app()->end();
-        }
-        catch (\Exception $ex) {
-            die($ex->getMessage());
-        }
-    }
+	// Actions
+	/**
+	 *
+	 */
+	public function actionStream()
+	{
+		$path = ( isset( $_GET['path'] ) ? $_GET['path'] : '' );
+		try
+		{
+			if ( 0 === substr_compare( $path, 'web-core/', 0, 9, true ) )
+			{
+				$key = Yii::app()->basePath . DIRECTORY_SEPARATOR . '..' . DIRECTORY_SEPARATOR;
+				$key .= 'public' . DIRECTORY_SEPARATOR . $path;
+				if ( is_file( $key ) )
+				{
+					$ext = FileUtilities::getFileExtension( $key );
+					$result = file_get_contents( $key );
+					header( 'Last-Modified: ' . gmdate( 'D, d M Y H:i:s \G\M\T', filemtime( $key ) ) );
+					header( 'Content-type: ' . FileUtilities::determineContentType( $ext, '', $key ) );
+					header( 'Content-Length:' . filesize( $key ) );
+					$disposition = 'inline';
+					header( "Content-Disposition: $disposition; filename=\"$path\";" );
+					echo $result;
+				}
+				else
+				{
+					$status_header = "HTTP/1.1 404 The specified file '$path' does not exist.";
+					header( $status_header );
+					header( 'Content-type: text/html' );
+				}
+			}
+			else
+			{
+				$app = ServiceHandler::getInstance()->getServiceObject( 'lib' );
+				$app->streamFile( $path );
+			}
+			Yii::app()->end();
+		}
+		catch ( \Exception $ex )
+		{
+			die( $ex->getMessage() );
+		}
+	}
 
-    /**
-     *
-     */
-    public function actionIndex()
-    {
-        Yii::app()->end();
-    }
-
+	/**
+	 *
+	 */
+	public function actionIndex()
+	{
+		Yii::app()->end();
+	}
 }
