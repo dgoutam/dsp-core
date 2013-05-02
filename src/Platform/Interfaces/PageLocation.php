55ead68eabf7b21503c4a88cd394aa34952b9105
Jerry Ablan <jerryablan@dreamfactory.com>
2013-05-02 12:14:15 -0400

    fix

--- src/Platform/Interfaces/PageLocation.php
+++ src/Platform/Interfaces/PageLocation.php
@@ -17,6 +17,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+namespace Platform\Interfaces;
 /**
  * PageLocation.php
  * The various places within a web page
