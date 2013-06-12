962d50cbf2f5fbedf64900f4c61d3d0c70caa084
Lee Hicks <leehicks@dreamfctory.com>
2013-06-12 13:14:11 -0400

    bump the rev to 1.0.4

--- config/common.config.php
+++ config/common.config.php
@@ -29,7 +29,7 @@
 /**
  * @var string
  */
-const DSP_VERSION = '1.0.3';
+const DSP_VERSION = '1.0.4';
 const API_VERSION = '1.0';
 
 //	The base path of the project, where it's checked out basically
