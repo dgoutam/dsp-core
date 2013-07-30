f56b06ced9e0e7ec77c74439d84cd2543ee63c2d
Jerry Ablan <jerryablan@gmail.com>
2013-07-30 16:06:07 -0400

    Pass through remote login config to launchpad

--- config/common.config.php
+++ config/common.config.php
@@ -22,7 +22,6 @@ use DreamFactory\Platform\Utility\Fabric;
  * common.config.php
  * This file contains any application-level parameters that are to be shared between the background and web services
  */
-
 //*************************************************************************
 //* Constants
 //*************************************************************************
@@ -83,10 +82,7 @@ $_logFileName = basename( \Kisma::get( 'app.log_file' ) );
 /**
  * Aliases
  */
-if ( file_exists( __DIR__ . ALIASES_CONFIG_PATH ) )
-{
-	require __DIR__ . ALIASES_CONFIG_PATH;
-}
+file_exists( __DIR__ . ALIASES_CONFIG_PATH ) && require __DIR__ . ALIASES_CONFIG_PATH;
 
 /**
  * Application Paths
@@ -141,14 +137,20 @@ else
 return array_merge(
 	$_instanceSettings,
 	array(
-		 /** App Information */
+		 /**
+		  * App Information
+		  */
 		 'base_path'                  => $_basePath,
-		 /** DSP Information */
+		 /**
+		  * DSP Information
+		  */
 		 'dsp.version'                => DSP_VERSION,
 		 'dsp.name'                   => $_instanceSettings['dsp_name'],
 		 'dsp.auth_endpoint'          => DEFAULT_INSTANCE_AUTH_ENDPOINT,
 		 'cloud.endpoint'             => DEFAULT_CLOUD_API_ENDPOINT,
-		 /** External Logins */
+		 /**
+		  * External Logins
+		  */
 		 'dsp.allow_remote_logins'    => true,
 		 'dsp.remote_login_providers' => array(
 			 'facebook',
@@ -160,14 +162,20 @@ return array_merge(
 		  */
 		 'blobStorageConfig'          => $_blobConfig,
 		 'adminEmail'                 => DEFAULT_SUPPORT_EMAIL,
-		 /** The default service configuration */
+		 /**
+		  * The default service configuration
+		  */
 		 'dsp.service_config'         => require( __DIR__ . SERVICES_CONFIG_PATH ),
-		 /** Default services provided by all DSPs */
+		 /**
+		  * Default services provided by all DSPs
+		  */
 		 'dsp.default_services'       => array(
 			 array( 'api_name' => 'user', 'name' => 'User Login' ),
 			 array( 'api_name' => 'system', 'name' => 'System Configuration' ),
 		 ),
-		 /** The default application to start */
+		 /**
+		  * The default application to start
+		  */
 		 'dsp.default_app'            => '/public/launchpad/index.html',
 	)
 );
