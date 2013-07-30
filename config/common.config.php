3e9c65e74ff14451ac15cce38d0f88d82fcfe688
Jerry Ablan <jerryablan@gmail.com>
2013-07-30 15:51:44 -0400

    Added remote login support in config

--- config/common.config.php
+++ config/common.config.php
@@ -142,25 +142,32 @@ return array_merge(
 	$_instanceSettings,
 	array(
 		 /** App Information */
-		 'base_path'            => $_basePath,
+		 'base_path'                  => $_basePath,
 		 /** DSP Information */
-		 'dsp.version'          => DSP_VERSION,
-		 'dsp.name'             => $_instanceSettings['dsp_name'],
-		 'dsp.auth_endpoint'    => DEFAULT_INSTANCE_AUTH_ENDPOINT,
-		 'cloud.endpoint'       => DEFAULT_CLOUD_API_ENDPOINT,
+		 'dsp.version'                => DSP_VERSION,
+		 'dsp.name'                   => $_instanceSettings['dsp_name'],
+		 'dsp.auth_endpoint'          => DEFAULT_INSTANCE_AUTH_ENDPOINT,
+		 'cloud.endpoint'             => DEFAULT_CLOUD_API_ENDPOINT,
+		 /** External Logins */
+		 'dsp.allow_remote_logins'    => true,
+		 'dsp.remote_login_providers' => array(
+			 'facebook',
+			 'github',
+			 'stackoverflow',
+		 ),
 		 /**
 		  * User data
 		  */
-		 'blobStorageConfig'    => $_blobConfig,
-		 'adminEmail'           => DEFAULT_SUPPORT_EMAIL,
+		 'blobStorageConfig'          => $_blobConfig,
+		 'adminEmail'                 => DEFAULT_SUPPORT_EMAIL,
 		 /** The default service configuration */
-		 'dsp.service_config'   => require( __DIR__ . SERVICES_CONFIG_PATH ),
+		 'dsp.service_config'         => require( __DIR__ . SERVICES_CONFIG_PATH ),
 		 /** Default services provided by all DSPs */
-		 'dsp.default_services' => array(
+		 'dsp.default_services'       => array(
 			 array( 'api_name' => 'user', 'name' => 'User Login' ),
 			 array( 'api_name' => 'system', 'name' => 'System Configuration' ),
 		 ),
 		 /** The default application to start */
-		 'dsp.default_app'      => '/public/launchpad/index.html',
+		 'dsp.default_app'            => '/public/launchpad/index.html',
 	)
 );
