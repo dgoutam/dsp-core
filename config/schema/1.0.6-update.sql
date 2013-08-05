ad5aa6d83dda7d03cfaca1299fbb8cf139ab2956
Jerry Ablan <jerryablan@dreamfactory.com>
2013-08-05 14:56:57 -0400

    Updated schemas

--- config/schema/1.0.6-update.sql
+++ config/schema/1.0.6-update.sql
@@ -23,6 +23,7 @@
 
 DROP TABLE IF EXISTS `df_sys_account_provider`;
 DROP TABLE IF EXISTS `df_sys_service_account`;
+DROP TABLE IF EXISTS `df_sys_portal_account`;
 
 -- Unique index on portal accounts
-CREATE UNIQUE INDEX ux_portal_user_account ON df_sys_portal_account ( user_id, provider_name, provider_user_id );
+CREATE UNIQUE INDEX undx_provider_user_provider_user_id ON df_sys_provider_user (user_id, provider_id, provider_user_id);
