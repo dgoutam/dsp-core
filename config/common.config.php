f9d33639dbab5ba27ae67c6ecedb334febbcf571
Lee Hicks <leehicks@dreamfactory.com>
2013-08-06 16:37:09 -0400

    version fixing and upgrade fix for previously installed apps

--- config/common.config.php
+++ config/common.config.php
@@ -29,7 +29,7 @@
 /**
  * @var string
  */
-const DSP_VERSION = '1.0.5';
+const DSP_VERSION = '1.0.6';
 const API_VERSION = '1.0';
 
 //	The base path of the project, where it's checked out basically
