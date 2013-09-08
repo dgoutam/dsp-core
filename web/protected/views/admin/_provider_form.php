b2c7528ebcb99da6a9558a5e29165420cf055040
Jerry Ablan <jerryablan@dreamfactory.com>
2013-09-08 18:12:27 -0400

    Friday work

--- web/protected/views/admin/_provider_form.php
+++ web/protected/views/admin/_provider_form.php
@@ -2,25 +2,23 @@
 /**
  * _provider.form.php
  *
- * @var WebController $this
- * @var array         $schema
- * @var array         $resource
- * @var array         $_formOptions Provided by includer
- * @var array         $errors       Errors if any
+ * @var WebController           $this
+ * @var array                   $schema
+ * @var BasePlatformSystemModel $model
+ * @var array                   $_formOptions Provided by includer
+ * @var array                   $errors       Errors if any
+ * @var string                  $resourceName The name of this resource (i.e. App, AppGroup, etc.) Essentially the model name
+ * @var string                  $displayName
  */
+use DreamFactory\Platform\Yii\Models\BasePlatformSystemModel;
 use DreamFactory\Yii\Utility\BootstrapForm;
 use Kisma\Core\Utility\Bootstrap;
+use Kisma\Core\Utility\Inflector;
 use Kisma\Core\Utility\Option;
-
-$this->setBreadcrumbs(
-	array(
-		 'Providers' => 'providers/index',
-		 'Update'    => false,
-	)
-);
 
 //@TODO error handling from resource request
 $_errors = isset( $errors ) ? Option::clean( $errors ) : array();
+$_update = !$model->isNewRecord;
 
 if ( !empty( $_errors ) )
 {
@@ -42,34 +40,33 @@ if ( !empty( $_errors ) )
 HTML;
 }
 
-$_hashedId = $this->hashId( $resource['id'] );
+$_hashedId = $model->isNewRecord ? null : $this->hashId( $model->id );
 
 $_form = new BootstrapForm(
 	Bootstrap::Horizontal,
 	array(
-		 'id'             => 'update-provider',
+		 'id'             => 'update-resource',
 		 'method'         => 'POST',
-		 'x_editable_url' => '/admin/provider/update',
+		 'x_editable_url' => '/admin/' . $resourceName . '/update',
 		 'x_editable_pk'  => $_hashedId,
+		 'prefix'         => $resourceName,
 	)
 );
 
-$_newRecord = ( null === ( $_resourceId = Option::get( $resource, 'id' ) ) );
-
-$_form->setFormData( $resource );
+$_form->setFormData( $model );
 
 $_fields = array(
 	'Basic Settings' => array(
 		'api_name'      => array(
 			'type'        => 'text',
-			'class'       => $_newRecord ? 'required' : 'uneditable-input',
+			'class'       => !$_update ? 'required' : 'uneditable-input',
 			'placeholder' => 'How to address this provider via REST',
 			'hint'        => 'The URI portion to be used when calling this provider. For example: "github", or "facebook".',
 			'maxlength'   => 64,
 		),
 		'provider_name' => array(
 			'type'      => 'text',
-			'class'     => 'required' . ( !$_newRecord ? ' x-editable' : null ),
+			'class'     => 'required' . ( $_update ? ' x-editable' : null ),
 			'hint'      => 'The real name, or "display" name for this provider.',
 			'maxlength' => 64,
 		),
@@ -88,24 +85,10 @@ $_fields = array(
 );
 ?>
 <div class="row-fluid" style="border-bottom:1px solid #ddd">
-	<div class="span8">
-		<h2 style="margin-bottom: 0"><?php echo $_newRecord ? 'New Provider' : $resource['provider_name']; ?>
-			<small>Edit Provider</small>
+	<div class="pull-right">
+		<h2 style="margin-bottom: 0"><?php echo $displayName ?>
+			<small><?php echo( $_update ? 'Edit' : 'New' ); ?></small>
 		</h2>
-	</div>
-	<div class="span4" style="margin-top:10px">
-		<div class="pull-right" style="display: inline-block;">
-			<form method="POST" id="form-button-bar" style="display:inline;">
-				<input type="hidden" name="backup_instance" value="0">
-				<button data-row-id="<?php echo $_hashedId; ?>" class="btn btn-success" id="backup-instance">Backup</button>
-				<input type="hidden" name="restore_instance" value="0">
-				<button data-row-id="<?php echo $_hashedId; ?>" class="btn btn-info" id="restore-instance">Restore</button>
-				<input type="hidden" name="wipe_instance" value="0">
-				<button data-row-id="<?php echo $_hashedId; ?>" class="btn btn-warning" id="wipe-instance">Wipe</button>
-				<input type="hidden" name="delete_instance" value="0">
-				<button data-row-id="<?php echo $_hashedId; ?>" class="btn btn-danger" id="delete-instance">Delete</button>
-			</form>
-		</div>
 	</div>
 </div>
 
@@ -114,6 +97,17 @@ $_fields = array(
 		<form id="update-platform" method="POST" class="form-horizontal tab-form" action>
 			<?php $_form->renderFields( $_fields ); ?>
 		</form>
+	</div>
+</div>
+
+<div class="row-fluid">
+	<div class="span12" style="margin-top:10px">
+		<div class="pull-right" style="display: inline-block;">
+			<form method="POST" id="form-button-bar" style="display:inline;">
+				<button class="btn btn-success btn-primary" id="save-resource"> Save</button>
+				<button class="btn btn-danger" id="delete-instance"> Delete</button>
+			</form>
+		</div>
 	</div>
 </div>
 
@@ -136,8 +130,7 @@ jQuery(function($) {
 			success:  function(data) {
 				if (data && data.success) {
 					alert('Your provider request has been queued.');
-				}
-				else {
+				} else {
 					alert('There was an error: ' + data.details.message);
 				}
 			}
--- web/protected/views/admin/update.php
+++ web/protected/views/admin/update.php
@@ -1,8 +1,13 @@
 <?php
 /**
- * @var WebController $this
- * @var array         $resource
- * @var array         $schema
+ * @var WebController           $this
+ * @var array                   $schema
+ * @var BasePlatformSystemModel $model
+ * @var array                   $_formOptions Provided by includer
+ * @var array                   $errors       Errors if any
+ * @var string                  $resourceName The name of this resource (i.e. App, AppGroup, etc.) Essentially the model name
+ * @var string                  $displayName
+ * @var array                   $_data_
  */
 use DreamFactory\Yii\Utility\BootstrapForm;
 
@@ -12,12 +17,13 @@ $_form = new BootstrapForm();
 
 $_options = array(
 	'breadcrumbs' => array(
-		'Admin Dashboard' => '/admin',
-		'Resource'        => false,
+		'Admin Dashboard'   => '/admin',
+		$resourceName . 's' => '/admin#tab-' . strtolower( $resourceName ),
+		$displayName        => false,
 	)
 );
 
 $_formOptions = $_form->pageHeader( $_options );
 
 //	Render the form
-require __DIR__ . '/_provider.form.php';
+$this->renderPartial( '_provider_form', $_data_ );
