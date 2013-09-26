bd2ed37e194d12f9cc6b554532ee32f43b33c1fb
Jerry Ablan <jerryablan@gmail.com>
2013-09-26 19:24:31 -0400

    Updates to ensure storage_type_id and type_id are set. Added SALESFORCE service.

--- config/services.config.php
+++ config/services.config.php
@@ -92,7 +92,10 @@ return
 		PlatformServiceTypes::REMOTE_WEB_SERVICE   => array(
 			'class' => 'DreamFactory\\Platform\\Services\\RemoteWebSvc',
 		),
-		PlatformServiceTypes::SALESFORCE_SERVICE => array(
+		PlatformServiceTypes::SALESFORCE           => array(
+			'class' => 'DreamFactory\\Platform\\Services\\SalesforceDbSvc',
+		),
+		PlatformServiceTypes::SALESFORCE_SERVICE   => array(
 			'class' => 'DreamFactory\\Platform\\Services\\SalesforceDbSvc',
 		),
 	);
